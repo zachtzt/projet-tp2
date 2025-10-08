3db19584c7e9ac7b84ffaa59aa941c028564aed0
Zachary Lacasse <zachary_lacasse@outlook.com>
2025-10-08 15:09:16 -0400

    Borne et Transaction avancées

--- src/Borne.java
+++ src/Borne.java
@@ -1,4 +1,28 @@
 public class Borne {
     private Transaction transactionCourante;
     private double banqueDeLaBorne;
+
+    public Borne() {
+        this.transactionCourante = null;
+        this.banqueDeLaBorne = 0;
+    }
+
+    public void commencerTransaction(String placeStationnement){
+        if (placeStationnement.matches("G[0-9]{3}") || placeStationnement.matches("SQ[0-9]{3}")){
+            this.transactionCourante = new Transaction(placeStationnement);
+        } else {
+            System.out.println("Erreur ! Veuillez réessayer avec une place de stationnement valide.");
+        }
+    }
+    public void terminerTransactionCourante(){
+        this.banqueDeLaBorne += this.transactionCourante.getMontant();
+        this.transactionCourante = null;
+    }
+    public void genererRapport(){
+        System.out.println("-----------------------------------------------");
+        System.out.println("Argent dans la Borne : " + this.banqueDeLaBorne);
+        System.out.println("-----------------------------------------------");
+        System.out.println("Le solde de cette banque sera remis à 0,00$");
+        this.banqueDeLaBorne = 0;
+    }
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -2,4 +2,39 @@ public class Transaction {
     private String placeStationnement;
     private int dureeMinutes;
     private double montant;
+
+    private static final int MAX_DUREE_PARKING = 120;
+    private static final int MIN_DUREE_PARKING = 0;
+    private static final int INTERVAL_MINUTES = 15;
+
+    public Transaction(String placeStationnement) {
+        this.placeStationnement = placeStationnement;
+        this.dureeMinutes = MIN_DUREE_PARKING;
+        this.montant = 0;
+    }
+
+    public int getDureeMinutes() {
+        return dureeMinutes;
+    }
+
+    public void ajouterQuinzeMinutes(){
+        if (this.dureeMinutes < MAX_DUREE_PARKING){
+            this.dureeMinutes += INTERVAL_MINUTES;
+
+        }
+        else
+            System.out.println("Erreur ! Durée maximale de parking est de ");
+    }
+    public void retirerQuinzeMinutes(){
+        if (this.dureeMinutes >= MIN_DUREE_PARKING)
+            this.dureeMinutes -= INTERVAL_MINUTES;
+    }
+
+    public double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(double montant) {
+        this.montant = montant;
+    }
 }
